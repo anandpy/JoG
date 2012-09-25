c577e4a9b1a8fe98be536cd8afa1478431f3491c
sammy <samarth@actwitty.com>
2012-09-26 04:44:41 +0530

    some changes to css

--- public/javascript/view/jog_leaderboard_panel_view.js
+++ public/javascript/view/jog_leaderboard_panel_view.js
@@ -77,7 +77,8 @@ var JogLeaderboardView = {
     {
         var html = "";
           
-        html = html + '<div id="jog_data_posts_leaderboard_list">';
+        html = html + '<div id="jog_data_posts_leaderboard_list">' +
+                      '<h1>Leaderboard</h1>';
 
         $.each(data, function(index, post) { 
             html = html + JogLeaderboardView.postHtml(post);
@@ -99,6 +100,7 @@ var JogLeaderboardView = {
                    '<span>'+data.user_name+'</span>';
         }
 
+        var truncateTextLength = (data.post_pic && data.post_pic !== "" ) ? 200 : 300; ;
         var imgHtml = (data.post_pic && data.post_pic !== "" ) ? '<img src="'+data.post_pic+'" align="right">' : ""; 
 
         var html = '<div class="jog_data_posts_leaderboard_list_box" >'+
@@ -106,13 +108,13 @@ var JogLeaderboardView = {
                         userInfo()+
                         '</div>'+
                         '<div class="jog_data_posts_leaderboard_list_box_content">'+ 
-                            '<h5>'+data.date+'</h5>'+
+                            '<h5>2nd Oct 2012</h5>'+
                             '<h3 class="jog_data_posts_box_title">'+data.post_title +'</h3>'+
                             '<div class="jog_data_posts_box_content">'+
                                 imgHtml+
-                                data.post_text+
+                                JOG.utils.truncateText(data.post_text,truncateTextLength) +
                             '</div>'+
-                            '<div class="jog_data_posts_box_metric">'+
+                            '<div class="jog_data_posts_leaderboard_list_box_metric">'+
                                 '<div class="jog_data_posts_box_metric_vote_action"> Vote </div>'+
                                 '<div class="jog_data_posts_box_metric_vote_count"> '+data.vote_count+' Votes  </div>'+
                             '</div>'+
