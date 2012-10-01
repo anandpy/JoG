b51d7fe0ba1aab9dec5e2b09f86803573b4c3e3c
sammy <samarth@actwitty.com>
2012-10-01 12:52:06 +0530

    landing page ux

--- public/javascript/view/jog_landing_leaderboard_panel_view.js
+++ public/javascript/view/jog_landing_leaderboard_panel_view.js
@@ -34,7 +34,7 @@ var JogLandingLeaderboardPanelView = {
         
         var slide_hide_class = (index > 1) ? "slide_hide" : "no-class";
 
-        html = //'<a href="http://www.flickr.com/photos/jliba/4665625073/" id="jog_lp_slide_index_'+index+'" class="jog_slider '+slide_hide_class+'" target="_blank">'+
+        /*html = //'<a href="http://www.flickr.com/photos/jliba/4665625073/" id="jog_lp_slide_index_'+index+'" class="jog_slider '+slide_hide_class+'" target="_blank">'+
                     '<div class="jog_slider jog_lp_sp_main_container '+slide_hide_class+'" id="jog_lp_slide_index_'+index+'">'+  
                         '<img class="jog_lp_sp_curl" src="/images/curl.png">'+
                         '<div class="jog_lp_sp_user_name">'+
@@ -59,7 +59,32 @@ var JogLandingLeaderboardPanelView = {
                         '</div>'+
                     '</div>';
                 //'</a>';
+        */
+        html = //'<a href="http://www.flickr.com/photos/jliba/4665625073/" id="jog_lp_slide_index_'+index+'" class="jog_slider '+slide_hide_class+'" target="_blank">'+
+                    '<div class="jog_slider jog_lp_sp_main_container '+slide_hide_class+'" id="jog_lp_slide_index_'+index+'">'+  
+                        '<div class="jog_lp_sp_user_name">'+
+                            '<h5>'+data.user_name+'</h5>'+
+                        '</div>'+
+                        '<div class="jog_lp_sp_content">'+
+                            '<div class="jog_lp_sp_leftside">'+
+                                '<div class="jog_lp_sp_user_photo">'+
+                                    '<img src="'+data.user_pic+'">'+
+                                '</div>'+
+                                '<div class="jog_lp_sp_vote">'+
+                                    '<div class="jog_lp_sp_vote_count">'+data.votes_count+' Votes</div>'+
+                                    //'<div class="jog_lp_sp_vote_action"></div>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="jog_lp_sp_rightside">'+
+                                '<div class="jog_lp_sp_msg">'+JOG.utils.truncateText(data.post_title,20)+'</div>'+
+                                '<p class="jog_lp_sp_text">'+JOG.utils.truncateText(data.post_text,100)+'</p>'+
+                                '<div class="jog_lp_sp_by"></div>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>';
+                //'</a>';
         
+
     	return html;
 	},
 
